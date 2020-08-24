87a819ab9b646272248ab6ee5c0addbf16d8676d
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2020-08-24 08:35:08 +0800

    Fully replaced chakra ui with basic avant-bootstrap ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,28 @@
 import React, { FC } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { ChakraProvider, CSSReset } from '@chakra-ui/core'
 
-import { customTheme } from './theme'
 import { CategoriesPage, HomePage, IncomesPage, ExpensesPage, ReportPage } from './pages'
 
 export const App: FC = () => {
   return (
-    <ChakraProvider theme={customTheme}>
-      <CSSReset />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/expenses">
-            <ExpensesPage />
-          </Route>
-          <Route path="/report">
-            <ReportPage />
-          </Route>
-          <Route path="/categories">
-            <CategoriesPage />
-          </Route>
-          <Route path="/incomes">
-            <IncomesPage />
-          </Route>
-        </Switch>
-      </Router>
-    </ChakraProvider>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route path="/expenses">
+          <ExpensesPage />
+        </Route>
+        <Route path="/report">
+          <ReportPage />
+        </Route>
+        <Route path="/categories">
+          <CategoriesPage />
+        </Route>
+        <Route path="/incomes">
+          <IncomesPage />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import 'src/assets/bootstrap.min.css'
+import 'src/assets/avantui.min.css'
+
 import { App } from './App'
 
 const rootElement = document.querySelector('#root')
