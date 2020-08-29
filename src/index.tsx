42aaa411e9cd86b07d6e8871624f195b735dfa53
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2020-08-29 20:40:59 +0800

    Manifest

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,12 @@ import 'src/assets/avantui.min.css'
 import 'src/assets/corrections.css'
 
 import { App } from './App'
+import * as serviceWorker from './serviceWorker'
 
 const rootElement = document.querySelector('#root')
 render(<App />, rootElement)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
