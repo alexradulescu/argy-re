b9570eef6cbc85c7f3478f0217bdf15f11c296bc
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2020-08-29 19:23:05 +0800

    :bug: Cleanup of old remnants to latest version

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -2,8 +2,7 @@ import React, { FC } from 'react'
 import { NavLink } from 'react-router-dom'
 import { AiOutlineHome } from 'react-icons/ai'
 import { GiPayMoney, GiReceiveMoney } from 'react-icons/gi'
-import { RiSettings4Line } from 'react-icons/ri'
-import { HiOutlineDocumentReport } from 'react-icons/hi'
+import { RiSettings4Line, RiProfileLine } from 'react-icons/ri'
 
 export const Navigation: FC = () => (
   <nav className="nav justify-content-around flex-nowrap">
@@ -27,7 +26,7 @@ export const Navigation: FC = () => (
 
     <div className="nav-item">
       <NavLink className="nav-link text-center" activeClassName="active" to="/report">
-        <HiOutlineDocumentReport /> <small>Reports</small>
+        <RiProfileLine /> <small>Reports</small>
       </NavLink>
     </div>
 
